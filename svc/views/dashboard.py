1dc916d445573624af7c0b10a6971e82f08dd461
Aquib Jawed <jawed.aquib42@gmail.com>
2024-08-09 19:27:44 +0530

    Insight updated

--- svc/views/dashboard.py
+++ svc/views/dashboard.py
@@ -1,8 +1,6 @@
-from django.http import JsonResponse
 from django.utils import timezone
-from django.views.decorators.http import require_GET
 from django.views.generic import TemplateView
-from svc.views.insights import get_daily_insights
+from svc.views.insights import get_insights
 
 
 class DashboardView(TemplateView):
@@ -10,13 +8,21 @@ class DashboardView(TemplateView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        today = timezone.now().date()
+        period = self.request.GET.get('period', 'daily')
+        selected_date = self.request.GET.get('date')
+
+        if selected_date:
+            selected_date = timezone.datetime.strptime(selected_date, "%Y-%m-%d").date()
+        else:
+            selected_date = timezone.now().date()
 
         # Get insights
-        insights = get_daily_insights(today)
+        insights = get_insights(selected_date, period)
 
         # Update context with insights
         context.update(insights)
         context['today'] = timezone.now()
+        context['selected_date'] = selected_date
+        context['period'] = period
 
         return context
--- svc/views/insights.py
+++ svc/views/insights.py
@@ -1,33 +1,53 @@
 from django.db.models import Sum
+from datetime import timedelta
 from svc.models import Job, Expense, Service, JobItem, PurchaseOrder
 
 
-def get_daily_insights(date):
-    machining_revenue = \
-        Service.objects.filter(service_type='Machining', job__job_completion_time__date=date).aggregate(Sum('service_cost'))[
-            'service_cost__sum'] or 0
+def get_insights(date=None, period='daily'):
+    if period == 'weekly':
+        start_date = date - timedelta(days=date.weekday())  # Start of the week
+        end_date = start_date + timedelta(days=6)  # End of the week
+    elif period == 'monthly':
+        start_date = date.replace(day=1)  # Start of the month
+        next_month = start_date.replace(month=date.month % 12 + 1, day=1)
+        end_date = next_month - timedelta(days=1)  # End of the month
+    else:  # daily
+        start_date = end_date = date
 
-    workshop_revenue = \
-        Service.objects.filter(service_type='Workshop', job__job_completion_time__date=date).aggregate(Sum('service_cost'))[
-                                 'service_cost__sum'] or 0
+    machining_revenue = Service.objects.filter(
+        service_type='Machining',
+        job__job_completion_time__date__range=[start_date, end_date]
+    ).aggregate(Sum('service_cost'))['service_cost__sum'] or 0
 
-    total_service_revenue = Job.objects.filter(job_completion_time__date=date).aggregate(Sum('total_service_cost'))[
-                                'total_service_cost__sum'] or 0
+    workshop_revenue = Service.objects.filter(
+        service_type='Workshop',
+        job__job_completion_time__date__range=[start_date, end_date]
+    ).aggregate(Sum('service_cost'))['service_cost__sum'] or 0
 
-    total_item_revenue = Job.objects.filter(job_completion_time__date=date).aggregate(Sum('total_item_cost'))[
-                             'total_item_cost__sum'] or 0
+    total_service_revenue = Job.objects.filter(
+        job_completion_time__date__range=[start_date, end_date]
+    ).aggregate(Sum('total_service_cost'))['total_service_cost__sum'] or 0
+
+    total_item_revenue = Job.objects.filter(
+        job_completion_time__date__range=[start_date, end_date]
+    ).aggregate(Sum('total_item_cost'))['total_item_cost__sum'] or 0
 
     total_profit_by_item = sum(
         (job_item.item_unit_price - job_item.item.cost_price) * job_item.item_quantity
-        for job_item in JobItem.objects.filter(job__job_completion_time__date=date)
+        for job_item in JobItem.objects.filter(job__job_completion_time__date__range=[start_date, end_date])
     )
 
-    total_revenue = Job.objects.filter(job_completion_time__date=date).aggregate(Sum('paid_amount'))[
-                             'paid_amount__sum'] or 0
+    total_revenue = Job.objects.filter(
+        job_completion_time__date__range=[start_date, end_date]
+    ).aggregate(Sum('paid_amount'))['paid_amount__sum'] or 0
 
-    total_expense = Expense.objects.filter(created_at__date=date).aggregate(Sum('amount'))['amount__sum'] or 0
+    total_expense = Expense.objects.filter(
+        created_at__date__range=[start_date, end_date]
+    ).aggregate(Sum('amount'))['amount__sum'] or 0
 
-    item_purchased = PurchaseOrder.objects.filter(created_at__date=date).aggregate(Sum('po_amount'))["po_amount__sum"] or 0
+    item_purchased = PurchaseOrder.objects.filter(
+        created_at__date__range=[start_date, end_date]
+    ).aggregate(Sum('po_amount'))['po_amount__sum'] or 0
 
     total_income = total_revenue - total_expense
 
@@ -41,5 +61,6 @@ def get_daily_insights(date):
         'total_revenue': total_revenue,
         'total_expense': total_expense,
         'total_income': total_income,
-
+        'start_date': start_date,
+        'end_date': end_date,
     }
